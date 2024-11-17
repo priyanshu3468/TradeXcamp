4313086a3ed40f90edba96156d55408507967220
pp <you@example.com>
2024-11-17 19:34:15 +0530

    Navbar created

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
             <input type="text" placeholder='Search...'
             className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-            <FaSearch className='text-slate-600' / >
+            <FaSearch className='text-slate-700' / >
         </form>
         <ul className='flex gap-4'>
            <Link to='/'> <li className='hidden sm:inline text-slate-200 hover:underline'>Home</li></Link>
