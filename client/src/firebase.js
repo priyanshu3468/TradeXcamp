160f33bb96ec9e106e4789bd46f32835f11f143d
pp <you@example.com>
2025-04-15 00:27:58 +0530

    Frontend Updated

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "tradexcamp.firebaseapp.com",
-  projectId: "tradexcamp",
-  storageBucket: "tradexcamp.firebasestorage.app",
-  messagingSenderId: "839958175640",
-  appId: "1:839958175640:web:70039828448f5b716855ed"
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId:"",
 };
 
-// Initialize Firebase
+
 export const app = initializeApp(firebaseConfig);
