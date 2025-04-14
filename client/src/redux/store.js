09418ad3d5c50cf589431f5155cdcb377da2bf97
pp <you@example.com>
2025-04-15 00:18:33 +0530

    Updated Redux Toolkit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,16 +1,19 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import userReducer from "./user/userSlice";
-import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({ user: userReducer });
 
 const persistConfig = {
-  key: "root",
+  key: 'root',
   storage,
   version: 1,
 };
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
@@ -18,4 +21,5 @@ export const store = configureStore({
       serializableCheck: false,
     }),
 });
+
 export const persistor = persistStore(store);
